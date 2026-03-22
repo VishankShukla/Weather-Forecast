093c891582b1e245795b61da391eef80cb47455e
Vishank Shukla <vishankshukla51@gmail.com>
2026-03-22 20:57:15 +0530

    working

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("hello");
+document.addEventListener('DOMContentLoaded',() => {
+    const cityInput = document.getElementById("city-input");
+    const getWeatherBtn = document.getElementById("get-weather-btn");
+    const weatherInfo = document.getElementById("weather-info");
+    const cityNameDisplay = document.getElementById("city-name");
+    const temperatureDisplay = document.getElementById("temperature");
+    const descriptionDisplay = document.getElementById("description");
+    const errorMessage = document.getElementById("error-message");
+    
+    getWeatherBtn.addEventListener('click',() => {
+        let city = cityInput.value.trim();
+        if(city == ""){
+            return;
+        } 
+    });
+
+    function fetchWeatherData(city){
+
+    }
+    function displayWeatherData(weatherData){
+        //display
+
+    }
+    function showError(){
+        weatherInfo.classList.add('hidden');
+        errorMessage.classList.remove('hidden')
+    }
+});
